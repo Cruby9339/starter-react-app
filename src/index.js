197b5464896b546afa1ffee089bdbb3dcec8de9b
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-11-02 13:56:02 -0500

    adds blog project

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a react component
 const App = () => {
-	const buttonText = 'Click Me!';
-	const style = {backgroundColor: 'blue', color: 'white'}
+	return (
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00PM</span>
+					</div>
+					<div className="text">Nice blog Post!</div>
+				</div>
+			</div>
+		</div>
+	);
+}
 
-	return 	<div>
-				<label className="label" htmlFor="name">Enter name:</label>
-				<input id="name" type="text"/>
-				<button style={style}>{buttonText}</button>
-			</div>;
-};
-
-
-
-// Take the react component and show it on the screen
-ReactDOM.render(
-	<App />,
-	document.querySelector('#root')
-
-);
+ReactDOM.render(<App/>, document.querySelector('#root'))
